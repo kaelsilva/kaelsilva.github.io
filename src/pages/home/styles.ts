2ffd8e4bffa6e6a4247090a85c217be388f83be7
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-09-09 14:42:18 -0300

    chore: add style to content

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,30 +1,57 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 export const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   margin: 0;
-  display: flex;
-  align-items: stretch;
-`;
-
-export const AuthorName = styled.div`
-  color: #f205cb;
-  font-size: 12vh;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 100vh;
 `;
 
 export const Content = styled.div`
   background-color: #0c0826;
   width: 768px;
+  margin: 0;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   @media only screen and (max-width: 768px) {
     width: 100%;
   }
+`;
+
+export const AuthorName = styled.div`
+  color: #f205cb;
+  font-size: 64px;
+  font-family: 'Audiowide', 'Roboto', sans-serif;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Greetings = styled.div`
+  color: #0388a6;
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: flex-end;
+  margin-right: 25%;
+`;
+
+export const Subtitle = styled.div`
+  font-family: 'Roboto', sans-serif;
+  color: #bf046b;
+  letter-spacing: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-self: flex-end;
+  margin-right: 25%;
 `;
 
 export const Background = styled.div`

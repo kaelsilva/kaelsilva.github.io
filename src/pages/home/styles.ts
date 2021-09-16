5aade1606c25e97e28e78345938f9499d09b25bd
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-09-16 14:55:50 -0300

    chore: remove unecessary styles

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -67,39 +67,6 @@ export const AnimationContainer = styled.div`
   align-items: center;
   justify-content: center;
   animation: ${AppearFromLeft} 2s;
-  form {
-    margin: 80px 0;
-    width: 340px;
-    text-align: center;
-  }
-  h1 {
-    margin-bottom: 24px;
-  }
-  a {
-    color: #f4ede8;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-    &:hover {
-      color: ${shade(0.2, '#f4ede8')};
-    }
-  }
-  > a {
-    color: #ff9000;
-    display: block;
-    margin-top: 24px;
-    text-decoration: none;
-    transition: color 0.2s;
-    display: flex;
-    align-items: center;
-    svg {
-      margin-right: 16px;
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
-    }
-  }
 `;
 
 export const MarketName = styled.div`

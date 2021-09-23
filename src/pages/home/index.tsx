5f1cba828a0e895265a0003718e3747377b385a1
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-09-23 13:46:42 -0300

    chore: add more text

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -174,7 +174,7 @@ const Home: React.FC = () => {
             <Paragraph>
               In my time during graduation, I’ve made projects with TypeScript,
               Node and React for Web. Other projects were made with Python, Java
-              and C. Also, I’ve made some search projects with MATLAB about
+              and C. Also, I’ve made some research projects with MATLAB about
               gravimetry inversion,
             </Paragraph>
           </Topic>
@@ -208,25 +208,27 @@ const Home: React.FC = () => {
         <Title>Skills</Title>
         <SkillsContainer>
           <Paragraph>
-            I’m initially focused in developing web apps with the Node, React
-            (including Next.js), Docker and AWS. However, I see no problem with
-            using a different tool that I’ve no contact with so far, let’s say
-            Azure, for example.
+            I’m initially focused in developing web apps with the{' '}
+            <strong>Node</strong>, <strong>React</strong> (including{' '}
+            <strong>Next.js</strong>), <strong>Docker</strong> and{' '}
+            <strong>AWS</strong>. However, I see no problem with using a
+            different tool that I’ve no contact with so far, let’s say{' '}
+            <strong>Azure</strong> for cloud, for example. I'm familiarized with
+            <strong>SQL</strong> and <strong>NoSQL</strong> databases, querying,
+            functions, procedures, ORMs for easy connection between applications
+            and databases and <strong>RESTful</strong> APIs.
           </Paragraph>
 
           <Paragraph>
-            I use primarly Linux distros, since it has so much helpful features
-            for developing and most of servers use it too. Therefore, I’m
-            familiar with shell scripts and pretty much of the Linux
-            peculiarities.
-          </Paragraph>
-
-          <Paragraph>
-            Also, I use Python for some specific scripts that allow me to
+            I use primarly <strong>Linux</strong> distros, since it has so much
+            helpful features for developing and most of servers use it too.
+            Therefore, I’m familiar with shell scripts and pretty much of the
+            <strong> Linux</strong> peculiarities. Also, I use{' '}
+            <strong>Python</strong> for some specific scripts that allow me to
             automate specific tasks that I want to do from time to time and
-            eventually MATLAB for some math. I’m also interested in
-            non-conventional projects that creates its own marketshare just
-            after existing.
+            eventually <strong>MATLAB</strong> for some math scripts,
+            processment and graphics. I’m also interested in non-conventional
+            projects that creates its own marketshare just after existing.
           </Paragraph>
         </SkillsContainer>
       </Container>

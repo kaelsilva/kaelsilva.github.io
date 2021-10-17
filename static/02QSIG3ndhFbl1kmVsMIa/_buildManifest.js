b8cc754cccac379b92d21c207a6df7665cff304e
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-17 18:30:07 +0000

    Deploying to gh-pages from @ kaelsilva/kaelsilva.github.io@76a1c941a5ae6d132e0d955195ba777e36771a5a 🚀

---
+++ static/02QSIG3ndhFbl1kmVsMIa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/2-20ee044f9ef284faacc3.js","static/chunks/pages/index-e29c7d31f146b7fbb4e9.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

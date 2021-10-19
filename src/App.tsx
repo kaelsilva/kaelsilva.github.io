ea427f3876903ccb6dd3fbac14fb6c072429cf14
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-19 16:03:08 -0300

    refactor: app uses components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
-import Home from './pages/home';
 
-const App: React.FC = () => <Home />;
+import GlobalStyles from './GlobalStyles';
+
+import Toolbar from './components/Toolbar';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyles />
+
+    <Toolbar />
+
+    <Home />
+    <About />
+    <Skills />
+    <Portfolio />
+    <Contact />
+  </>
+);
 
 export default App;

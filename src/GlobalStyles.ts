40e2731f238882d685a0f4fd00db138a9ff461d6
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-19 16:03:26 -0300

    refactor: add new global styles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: '-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif';
+    text-decoration: none;
+    color: #fff;
+  }
+`;
+
+export default GlobalStyle;

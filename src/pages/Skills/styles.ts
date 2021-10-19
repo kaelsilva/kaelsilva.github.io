5336bbe0f5df8baff6a913054cc3efa2813bd249
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-19 16:04:01 -0300

    refactor: add skills component and styles

---
+++ src/pages/Skills/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+  flex-wrap: wrap;
+
+  &#about {
+    background-color: #0c0826;
+  }
+
+  &#skills {
+    background-color: #0c0826;
+  }
+
+  &#portfolio {
+    background-color: #0c0826;
+  }
+
+  &#contact {
+    background-color: #0c0826;
+  }
+`;
+
+export const Title = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 32px;
+  color: #f205cb;
+  display: flex;
+  flex: 1;
+  align-self: center;
+  justify-content: center;
+  width: 100vw;
+  flex-basis: 100%;
+`;
+
+export const SkillsContainer = styled.article`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  padding-left: 5vw;
+`;
+
+export const Paragraph = styled.p`
+  color: #0388a6;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+
+  ${SkillsContainer} & {
+    margin-top: 5vh;
+    margin: 0.5vh 0 0 0;
+    padding: 0 5vw 0 0;
+    width: 30vw;
+  }
+`;

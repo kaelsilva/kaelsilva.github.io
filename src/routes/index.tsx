e64272bdf73775d23513722f15a005ce0de96804
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-21 13:31:01 -0300

    chore: use public url for routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,11 +9,19 @@ import Contact from '../pages/Contact';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/about" exact component={About} />
-    <Route path="/skills" exact component={Skills} />
-    <Route path="/portfolio" exact component={Portfolio} />
-    <Route path="/contact" exact component={Contact} />
+    <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
+    <Route path={`${process.env.PUBLIC_URL}/about`} exact component={About} />
+    <Route path={`${process.env.PUBLIC_URL}/skills`} exact component={Skills} />
+    <Route
+      path={`${process.env.PUBLIC_URL}/portfolio`}
+      exact
+      component={Portfolio}
+    />
+    <Route
+      path={`${process.env.PUBLIC_URL}/contact`}
+      exact
+      component={Contact}
+    />
   </Switch>
 );
 

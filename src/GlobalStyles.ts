1526b942a7fc926061d86e92980cee53e874e6f2
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-21 11:28:29 -0300

    fix: global style adds white color to multiple components no more

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  * {
+  body {
     padding: 0;
     margin: 0;
     font-family: '-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif';

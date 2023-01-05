6eac5c3b3efafa95c27460471fd75dcc62d77e37
Kael Silva <kaelsilvaaraujo@gmail.com>
2023-01-04 21:45:30 -0300

    feat: add audiowide and roboto fonts to global style

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,10 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
+import AudiowideRegular from './assets/fonts/Audiowide-Regular.ttf';
+import RobotoRegular from './assets/fonts/Roboto-Regular.ttf';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Audiowide';
+    src: local('Audiowide'),
+      url(${AudiowideRegular});
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    src: local('Roboto'),
+      url(${RobotoRegular});
+    font-display: fallback;
+  }
+
   body {
     padding: 0;
     margin: 0;
-    font-family: '-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif';
+    font-family: 'Roboto';
     text-decoration: none;
     color: #fff;
   }
